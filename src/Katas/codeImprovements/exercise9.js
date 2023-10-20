8eac1686fe0dbb27c3e9ad6345001c2e2af2a2d5
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-20 12:34:40 +0200

    add exercise9

--- src/Katas/codeImprovements/exercise9.js
+++ src/Katas/codeImprovements/exercise9.js
@@ -1,7 +1,6 @@
 function calculateMatrixSum(matrix) {
-    var sum = 0;
-    for (var i = 0; i < matrix.length; i++)
-      for (var j = 0; j < matrix[i].length; j++)
-        sum += matrix[i][j];
-    return sum;
-  }
+  return matrix.reduce(
+    (acc, row) => acc + row.reduce((rowSum, element) => rowSum + element, 0),
+    0
+  );
+}
